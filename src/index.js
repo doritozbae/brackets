86643f7083b5cbbb902a79627a75a9721b294274
doritozbae <doritosbae@mail.ru>
2021-08-30 03:27:06 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let leftBracket = [];
+  let rightBracket = []; 
+  let st = [];
+
+ for (let j = 0; j < bracketsConfig.length; j++) {
+   leftBracket.push(bracketsConfig[j][0]);
+   rightBracket.push(bracketsConfig[j][1]);
+ } 
+
+ let newStr = str.split(""); 
+
+ for (let i = 0; i < newStr.length; i++) {
+  //let currentIndex = newStr[i];
+    
+    // if (leftBracket.includes(currentIndex)) {
+    //   st.push(currentIndex);
+    // }
+    // else if (rightBracket.includes(currentIndex)) {
+    //   if (rightBracket.indexOf(currentIndex) != leftBracket.indexOf(st.pop())) {
+    //     return false;
+    //   } 
+    // }
+
+    if (rightBracket.includes(newStr[i])) {
+      if (st.includes(leftBracket[rightBracket.indexOf(newStr[i])])) {
+        st.pop();
+        continue;
+      }
+    }
+
+    if (leftBracket.includes(newStr[i])) {
+      st.push(newStr[i]);
+      continue;
+    }
+
+    return false;
+  }
+
+if (st.length > 0) {
+  return false;
+}
+return true;
+
+ 
 }
